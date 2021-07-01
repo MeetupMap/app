e394532f6ea8d64414b4dd52f7703b7f0072b982
warren yun <xxwarrenhdxx@gmail.com>
2021-07-01 17:43:40 -0400

    Put map in scaffold, added app bar

--- lib/main.dart
+++ lib/main.dart
@@ -25,32 +25,61 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<Marker> _locations = [];
+
+  void _addLocations() {
+    Marker l1 = new Marker(
+      width: 40.0,
+      height: 40.0,
+      point: LatLng(51.5, -0.09),
+      builder: (ctx) => Container(child: Icon(Icons.circle)),
+    );
+    _locations.insert(_locations.length, l1);
+  }
+
+  StatefulWidget _buildMap() {
+    return FlutterMap(
+        options: MapOptions(
+          center: LatLng(51.5, -0.09),
+          zoom: 20.0,
+        ),
+        layers: [
+          TileLayerOptions(
+              urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+              subdomains: ['a', 'b', 'c']),
+          MarkerLayerOptions(
+            markers: _locations,
+          ),
+        ]);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return FlutterMap(
-      options: MapOptions(
-        center: LatLng(51.5, -0.09),
-        zoom: 13.0,
-      ),
-      layers: [
-        TileLayerOptions(
-          urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-          subdomains: ['a', 'b', 'c']
-        ),
-        MarkerLayerOptions(
-        markers: [
-          Marker(
-            width: 80.0,
-            height: 80.0,
-            point: LatLng(51.5, -0.09),
-            builder: (ctx) =>
-            Container(
-              child: FlutterLogo(),
-            ),
+    return Scaffold(
+        appBar: AppBar(title: Text("Meetup Map")),
+        body: FlutterMap(
+          options: MapOptions(
+            center: LatLng(51.5, -0.09),
+            zoom: 20.0,
           ),
-        ],
-      ),
-      ]
-    );
+          layers: [
+            TileLayerOptions(
+                urlTemplate:
+                    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                subdomains: ['a', 'b', 'c']),
+            MarkerLayerOptions(
+              markers: [
+                Marker(
+                  width: 80.0,
+                  height: 80.0,
+                  point: LatLng(51.5, -0.09),
+                  builder: (ctx) => Container(
+                    child: Icon(Icons.circle),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ));
   }
 }
